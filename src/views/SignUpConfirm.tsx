b498653a48ecbf6c320fefd9dd94044211d961ae
Brian Vu <65921730+BrianVu999@users.noreply.github.com>
2021-10-07 20:20:40 -0400

    update ./src/views/SignUpConfirm.tsx file

--- src/views/SignUpConfirm.tsx
+++ src/views/SignUpConfirm.tsx
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
 import styled from "styled-components";
-import {
-    Grid,
-    Container,
-} from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import SignupImage from "../assets/images/signup-confirm-image.svg";
-import { BlueButton } from "../components/styles";
+import Button from "../components/common/Button";
 
 const SignUpConfirm = () => {
 
@@ -18,43 +13,40 @@ const SignUpConfirm = () => {
 
     return (
         <div>
-            <SignupContainer style={SignupContainerStyle}>
-                <Grid container>
+            <SignupContainer>
+                <FirstDiv>
 
-                    <Grid item xs={7}>
+                    <HeaderOne>Thanks for signing up!</HeaderOne>
 
-                        <HeaderOne>Thanks for signing up!</HeaderOne>
-                            <p style={HeaderTwo}>
-                                We will review your organization and notify you when your account is approved.
-                            </p>
+                    <HeaderTwo>
+                        We will review your organization and notify you when your account is approved.
+                    </HeaderTwo>
 
-                        <div style={ParaContainer}>
-                            <p style={ParaStyleOne}>
-                                For the safety of our student volunteers, wee ensure that all organization accounts are registered under the Government of Canada before allowing you to post opportunities.
-                            </p>
+                    <ParaContainer>
 
-                            <p style={ParaStyleOne}>
-                                For now, please help us speed up the verification process by checking your inbox to confirm your email.
-                            </p>
+                        <ParaStyleOne>
+                            For the safety of our student volunteers, wee ensure that all organization accounts are registered under the Government of Canada before allowing you to post opportunities.
+                        </ParaStyleOne>
 
-                            <p style={ParaStyleOne}>
-                                If you don’t see the email within 72 hours, please contact our support team for assistance.
-                            </p>
-                        </div>
+                        <ParaStyleOne>
+                            For now, please help us speed up the verification process by checking your inbox to confirm your email.
+                        </ParaStyleOne>
 
-                        <DashboardButton onClick={handleDashboardClick}>
-                            Return to home
-                        </DashboardButton>
-                    </Grid>
+                        <ParaStyleOne>
+                            If you don’t see the email within 72 hours, please contact our support team for assistance.
+                        </ParaStyleOne>
 
-                    <Grid item xs={5}>
-                        <ImageContainer>
-                            <img src={SignupImage} alt="SignupImage" style={SignupImageStyle} />
-                        </ImageContainer>
-                    </Grid>
+                    </ParaContainer>
 
-                </Grid>
+                    <DashboardButton onClick={handleDashboardClick}>
+                        Return to home
+                    </DashboardButton>
 
+                </FirstDiv>
+
+                <ImageContainer>
+                    <img src={SignupImage} alt="SignupImage" width="100%" />
+                </ImageContainer>
 
             </SignupContainer>
 
@@ -64,57 +56,49 @@ const SignUpConfirm = () => {
 
 export default SignUpConfirm;
 
-const SignupContainer = styled(Container)`
+const FirstDiv = styled.div`
+    display: inline-block;
+    width: 550px;
+`;
+
+const SignupContainer = styled.div`
     font-family: Source Sans Pro;
-    height: 100%;
+    width:fit-content;
+    margin: auto;
     padding-top: 50px;
 `;
 
-const ParaContainer: React.CSSProperties = {
-    fontSize: "0.9em",
-    width: "80%",
-    paddingLeft: "max-content"
-};
+const ParaContainer = styled.div`
+    font-size: 0.9em;
+    width: 440px;
+`;
 
-const HeaderTwo: React.CSSProperties = {
-    marginBottom: "40px",
-};
+const HeaderTwo = styled.p`
+    margin-bottom: 40px;
+`;
 
-const ParaStyleOne: React.CSSProperties = {
-    color: "rgba(115, 107, 107, 1)"
-};
+const ParaStyleOne = styled.p`
+    color: rgba(115, 107, 107, 1);
+`;
 
-export const HeaderOne = styled.h1`
+const HeaderOne = styled.h1`
     font-size: 40px;
     font-weight: bold;
     margin: 0;
 `;
 
-export const SubHeader = styled.h4`
+const SubHeader = styled.h4`
     font-size: 14px;
     font-weight: 400;
     color: #736b6b;
     margin: 9px 0 0 0;
 `;
 
+const DashboardButton = styled(Button)`
+    margin-top: 30px;
+`;
+
 const ImageContainer = styled.div`
-    margin-top: 20px;
-    text-align: right;
+    width: 400px;
+    display: inline-block;
 `;
-
-const DashboardButton = styled(BlueButton)`
-    width: 247px;
-    font-weight: bold;
-    font-size: 16px;
-    margin-right: 14px;
-    margin-top: 30px
-`;
-
-const SignupImageStyle: React.CSSProperties = {
-    width: "100%"
-};
-
-const SignupContainerStyle: React.CSSProperties = {
-    paddingLeft: "160px",
-    paddingRight: "160px"
-};
