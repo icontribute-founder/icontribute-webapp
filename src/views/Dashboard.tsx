bf0ed848e7fe474d2c968e7ba21384926c3bff09
James Ying <james.ying@gmail.com>
2021-10-14 13:27:58 -0400

    Playing with dropdown menu

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -5,115 +5,129 @@ import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import Button from "../components/common/Button";
 import InputField from "../components/common/InputField";
-import { Select, MenuItem } from "@material-ui/core";
-
+import { Select, MenuItem, FormControl, InputLabel } from "@material-ui/core";
+import SearchIcon from "../components/Svgs/SearchIcon";
 
 const Dashboard = () => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    
-    let [searchQuery, setSearchQuery] = useState("");
-    let [sortBy, setSortBy] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const handleOnClick = () => {
-        history.push("/new-opportunity");
-    };
+  let [searchQuery, setSearchQuery] = useState("");
+  let [sortBy, setSortBy] = useState("");
 
-    const handleSearchQueryChange = (e: any) => {
-        console.log("Search query: ", e);
-        //dispatch(updateSearchQuery(e.target.value));
-    };
+  const handleOnClick = () => {
+    history.push("/new-opportunity");
+  };
 
-    const handleSortByChange = (e: any) => {
-        console.log("Sort By: ", e);
-        //dispatch(updateSearchQuery(e.target.value));
-    };
+  const handleSearchQueryChange = (e: any) => {
+    console.log("Search query: ", e);
+    //dispatch(updateSearchQuery(e.target.value));
+  };
 
-    return (
-        <div>
-            <HeaderContainer>
-                <LeftBox>
-                    <HeaderOne>Your organization dashboard</HeaderOne>
-                    <HeaderThree>Here you can view the volunteer opportunities you've posted, edit them, or create a new one.</HeaderThree>
-                </LeftBox>
-                <RightBox>
-                    <Button onClick={handleOnClick}>Create a new opportunity</Button>
-                </RightBox>
-            </HeaderContainer>
+  const handleSortByChange = (e: any) => {
+    console.log("Sort By: ", e.target.value);
+    setSortBy(e.target.value);
+  };
 
-            <FilterContainer>                
-                <LeftBox>
-                    <InputField
-                        label=" "
-                        type="text"
-                        placeholder="Search events ..."
-                        name="search-query"
-                        id="search-query"
-                        value={searchQuery}
-                        onChange={handleSearchQueryChange}
-                    />
-                </LeftBox>
+  return (
+    <div>
+      <HeaderContainer>
+        <LeftBox>
+          <HeaderOne>Your organization dashboard</HeaderOne>
+          <HeaderThree>
+            Here you can view the volunteer opportunities you've posted, edit
+            them, or create a new one.
+          </HeaderThree>
+        </LeftBox>
+        <RightBox>
+          <Button onClick={handleOnClick}>Create a new opportunity</Button>
+        </RightBox>
+      </HeaderContainer>
 
-                <RightBox>
-                    <Select
-                        labelId="sort-by-select-label"
-                        id="sort-by-select"
-                        value={sortBy}
-                        label="Sort By"
-                        onChange={handleSortByChange}
-                    >
-                        <MenuItem value={1}>Distance (Closest)</MenuItem>
-                        <MenuItem value={2}>Date (Newest)</MenuItem>
-                    </Select>
-                </RightBox>
+      <FilterContainer>
+        {/*<SearchBox>
+          <SearchIcon />
+          <InputField
+            label=" "
+            type="text"
+            placeholder="Search events ..."
+            name="search-query"
+            id="search-query"
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+          />
+        </SearchBox>*/}
 
-            </FilterContainer>
-
-
-        </div>
-    );
+        <RightBox>
+          <FormControl variant="outlined">
+            <InputLabel id="sort-by-select-label">Sort By</InputLabel>
+            <Select
+              labelId="sort-by-select-label"
+              id="sort-by-select"
+              value={sortBy}
+              label="Sort By"
+              onChange={handleSortByChange}
+              autoWidth
+            >
+              <MenuItem value={1}>Distance (Closest)</MenuItem>
+              <MenuItem value={2}>Date (Newest)</MenuItem>
+            </Select>
+          </FormControl>
+        </RightBox>
+      </FilterContainer>
+    </div>
+  );
 };
 
 export default Dashboard;
 
 const HeaderContainer = styled.div`
-    font-family: Source Sans Pro;
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: space-between;
-    border-bottom: 2px solid silver;
-    flex-direction: row;
-    width: 1;
+  font-family: Source Sans Pro;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+  border-bottom: 2px solid silver;
+  flex-direction: row;
+  width: 1;
 `;
 
 const FilterContainer = styled.div`
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: flex-start;
-    flex-direction: row;
-    width: 1;
-    margin-left: 2%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-direction: row;
+  width: 1;
+  margin-left: 2%;
 `;
 
 const LeftBox = styled.div`
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    flex-direction: column;
-    width: 0.5;
-    padding-left: 2%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 0.5;
+  padding-left: 2%;
 `;
 
 const RightBox = styled.div`
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    flex-direction: column;
-    width: 0.5;
-    padding-top: 2%;
-    padding-right: 3%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 0.5;
+  padding-top: 2%;
+  padding-right: 3%;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: row;
+  margin-top: 0%;
+  padding-left: 0%;
 `;
 
 const HeaderOne = styled.h1`
