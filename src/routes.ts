20b90245de2f8dd51f9bd1e7d55230c5365ff136
Brian Vu <65921730+BrianVu999@users.noreply.github.com>
2021-10-18 19:56:31 -0400

    Update routes

--- src/routes.ts
+++ src/routes.ts
@@ -14,54 +14,54 @@ import SignUpConfirm from "./views/SignUpConfirm";
 import OrganizationDashboard from "./views/OrganizationDashboard";
 
 export const routes = [
-  // { path: "/dashboard", exact: true, component: Dashboard },
-  {
-    path: "/new-opportunity",
-    exact: true,
-    component: NewOpportunity,
-  },
-  {
-    path: "/",
-    exact: true,
-    component: Dashboard,
-  },
-  { path: "/profile", exact: true, component: Profile },
-  { path: "/confirmation1", exact: true, component: Confirmation1 },
-  { path: "/confirmation2", exact: true, component: Confirmation2 },
-  // { path: "/", exact: true, component: Login },
-  {
-    path: "/new-opportunity-review",
-    exact: true,
-    component: ReviewOpportunity,
-  },
-  {
-    path: "/new-opportunity-confirm",
-    exact: true,
-    component: OpportunityConfirmPage,
-  },
-  {
-    path: "/account-settings",
-    exact: true,
-    component: AccountSettings,
-  },
-  {
-    path: "/notifications",
-    exact: true,
-    component: Notifications,
-  },
-  {
-    path: "/signup",
-    exact: true,
-    component: SignUp,
-  },
-  {
-    path: "/signup-confirm",
-    exact: true,
-    component: SignUpConfirm,
-  },
-  {
-    path: "/organization-dashboard",
-    exact: true,
-    component: OrganizationDashboard,
-  },
+    // { path: "/dashboard", exact: true, component: Dashboard },
+    {
+        path: "/new-opportunity",
+        exact: true,
+        component: NewOpportunity,
+    },
+    {
+        path: "/",
+        exact: true,
+        component: Dashboard,
+    },
+    { path: "/profile", exact: true, component: Profile },
+    { path: "/confirmation1", exact: true, component: Confirmation1 },
+    { path: "/confirmation2", exact: true, component: Confirmation2 },
+    // { path: "/", exact: true, component: Login },
+    {
+        path: "/new-opportunity-review",
+        exact: true,
+        component: ReviewOpportunity,
+    },
+    {
+        path: "/new-opportunity-confirm",
+        exact: true,
+        component: OpportunityConfirmPage,
+    },
+    {
+        path: "/account-settings",
+        exact: true,
+        component: AccountSettings,
+    },
+    {
+        path: "/notifications",
+        exact: true,
+        component: Notifications,
+    },
+    {
+        path: "/signup",
+        exact: true,
+        component: SignUp,
+    },
+    {
+        path: "/signup-confirm",
+        exact: true,
+        component: SignUpConfirm,
+    },
+    {
+        path: "/organization-dashboard",
+        exact: true,
+        component: OrganizationDashboard,
+    },
 ];
