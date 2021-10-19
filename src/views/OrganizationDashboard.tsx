59d1732da19668adda4bdf187ae6d7abcf98374b
Brian Vu <65921730+BrianVu999@users.noreply.github.com>
2021-10-18 21:05:08 -0400

    Fix some errors in OrganizationDashboard.tsx

--- src/views/OrganizationDashboard.tsx
+++ src/views/OrganizationDashboard.tsx
@@ -5,30 +5,28 @@ import config from "../firebaseConfig.json";
 import { OpportunityCollection } from "../firebase-access/src/";
 
 const OrganizationDashboard = () => {
-    const [indexes, setIndexes] = useState([false]);
+    const [indexes, setIndexes] = useState([false, false]);
     const [opportunities, setOpportunity] = useState([]);
 
-    const defaultIndexesFromOpsLength: boolean[] = [];
+    const defaultIndexesBasedOnOpsLength: boolean[] = [];
 
     useEffect(() => {
         const getOpportunity = async () => {
             const opportunityCollection = OpportunityCollection.create(config);
             const data: any = await opportunityCollection.getOpportunities();
             setOpportunity(data);
-            console.log(data);
-            for (let i = 0; i < data.length; i++) {
-                defaultIndexesFromOpsLength[i] = false;
+
+            for (let i = 0; i < opportunities.length; i++) {
+                defaultIndexesBasedOnOpsLength[i] = false;
             }
-            setIndexes(defaultIndexesFromOpsLength);
+            setIndexes(defaultIndexesBasedOnOpsLength);
         };
         getOpportunity();
     }, []);
-
     const handleCardOnClick = (e: any, i: any) => {
-        const newIndexes = defaultIndexesFromOpsLength;
+        const newIndexes = [false, false];
         newIndexes[i] = true;
         setIndexes(newIndexes);
-
     };
 
     const ListEventCardComponents = opportunities.map((props: any, i) => (
@@ -36,7 +34,7 @@ const OrganizationDashboard = () => {
             eventImage={props.eventImage}
             eventName={props.eventName}
             date={new Date(parseInt(props.date.seconds))}
-            deadline={new Date(parseInt(props.deadline.seconds))}
+            deadline={new Date(props.deadline.seconds)}
             description={props.description}
             eventID={props.eventID}
             onClick={(e: any) => handleCardOnClick(e, i)}
@@ -47,7 +45,6 @@ const OrganizationDashboard = () => {
     return (
         <OrganizationDashboardPage>
             <EventsListContainer>{ListEventCardComponents}</EventsListContainer>
-            <EventDetails></EventDetails>
         </OrganizationDashboardPage>
     );
 };
@@ -57,16 +54,7 @@ const OrganizationDashboardPage = styled.div`
 `;
 
 const EventsListContainer = styled.div`
-    display: inline-block;
     width: 44%;
-    font-family: Source Sans Pro;
-    overflow-y: scroll;
-    height: 100%;
-`;
-
-const EventDetails = styled.div`
-    display: inline-block;
-    width: 56%;
     font-family: Source Sans Pro;
     overflow-y: scroll;
     height: 100%;
