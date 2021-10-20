27cde9a8a579d3e95dac1d45a68b8b75f76ce329
James Ying <james.ying@gmail.com>
2021-10-19 23:31:39 -0400

    Resolved other merge conflicts

--- src/firebase-access/tests/event.test.ts
+++ src/firebase-access/tests/event.test.ts
@@ -2,48 +2,6 @@ import { assert, expect } from "chai";
 import { GeoPoint } from "firebase/firestore";
 import { defaultEvent } from "../dist";
 
-<<<<<<< HEAD
-describe("Opportunity", function () {
-  const opportunityCollection = OpportunityCollection.create(config);
-  const companyId = "pPn3FypudrthuNbop1wh";
-  const eventId = "utJSlNLCImWrkTuxFpGM";
-
-  before((done) => {
-    opportunityCollection.connectFirestoreEmulator("localhost", 8080);
-    done();
-  });
-
-  it("should get some opportunities", async () => {
-    const opportunities = await opportunityCollection.getOpportunities();
-    assert(opportunities.length > 0);
-  });
-
-  it("should get one opportunity by the given id", async () => {
-    const opportunity = await opportunityCollection.getOpportunityById(eventId);
-    expect(opportunity).to.not.be.null;
-    if (opportunity !== null) {
-      assert(opportunity.id === eventId);
-    }
-  });
-
-  it("should create a new opportunity", async () => {
-    const event = {
-      ...defaultEvent,
-      eventName: "Test event",
-      description: "",
-      companyName: "",
-      email: "",
-      coordinates: new GeoPoint(36.66002, 6.18195),
-    };
-
-    const res = await opportunityCollection.createOpportunity(companyId, event);
-    assert(res);
-  });
-
-  it("should update an exisiting opportunity", async () => {
-    await opportunityCollection.updateOpportunity(eventId, {
-      companyName: "Test company name",
-=======
 export const opportunityTest = function () {
     const companyId = "testcompany@email.com";
     const eventId = "utJSlNLCImWrkTuxFpGM";
@@ -68,16 +26,9 @@ export const opportunityTest = function () {
         if (opportunity !== null) {
             assert(opportunity.id === eventId);
         }
->>>>>>> 0c7f13e5edf9049d45fcc68bd65191e02f191252
     });
   });
 
-<<<<<<< HEAD
-  it("should delete an exisiting opportunity", async () => {
-    await opportunityCollection.deleteOpportunity(eventId);
-  });
-});
-=======
     it("should create a new opportunity", async function () {
         const event = {
             ...defaultEvent,
@@ -105,4 +56,3 @@ export const opportunityTest = function () {
         await this.opportunityCollection.deleteOpportunity(eventId);
     });
 };
->>>>>>> 0c7f13e5edf9049d45fcc68bd65191e02f191252
