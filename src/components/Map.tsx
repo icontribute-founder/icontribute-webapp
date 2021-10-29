b2a2db08ffc8dc156394f14ab3ae51f7db5c04e3
Jiacheng Zhang <zhangjiacheng54@gmail.com>
2021-10-28 22:31:40 -0700

    clean up code and adjust some UI

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,59 +1,47 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
-const containerStyle = {
-    width: '700px',
-    height: '400px'
-  };
-  
-  const centered = {
-    lat: -3.745,
-    lng: -38.523
-  };
+interface MapProps {
+    center: { lat: number; lng: number };
+}
 
+const Map = ({ center }: MapProps) => {
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!,
+    });
 
+    const [map, setMap] = useState(null);
 
-  interface MapProps {
-    center: typeof centered;
-    
-  }
+    const onLoad = useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        setMap(map);
+    }, []);
 
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
 
-const Map = ({
-    center,
-  }: MapProps) => {
-
-const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!
-    })
-
-    const [map, setMap] = React.useState(null)
-
-    const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-    }, [])
-
-    const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-    }, [])
-  
-    console.log("This is center:", center)
+    // console.log("This is center:", center);
     return isLoaded ? (
         <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
+            mapContainerStyle={{
+                width: "100%",
+                height: "400px",
+            }}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            options={{ scrollwheel: false }}
         >
-          <Marker position={center} />
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
+            <Marker position={center} />
+            {/* Child components, such as markers, info windows, etc. */}
         </GoogleMap>
-    ) : <></>
+    ) : (
+        <></>
+    );
 };
-  
+
 export default Map;
--- src/views/OrganizationDashboard.tsx
+++ src/views/OrganizationDashboard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import React from "react";
 
 import { useHistory } from "react-router";
 import styled from "styled-components";
@@ -16,290 +15,244 @@ import { OpportunityCollection } from "../firebase-access/src/";
 import { useJsApiLoader } from "@react-google-maps/api";
 
 const OrganizationDashboard = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const [indexes, setIndexes] = useState([false, false]);
-  const [opportunities, setOpportunity] = useState([]);
+    const [selectedOpportunity, setSelectedOpportunity] = useState<any>(null);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [opportunities, setOpportunity] = useState([]);
 
-  const defaultIndexesBasedOnOpsLength: boolean[] = [];
+    const [center, setCenter] = useState({
+        lat: 45.42,
+        lng: -75.69,
+    });
 
-  const [positionName, setPositionName] = useState("");
-  const [companyName, setCompanyName] = useState("");
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState("");
-  const [eventImage, setEventImage] = useState("");
-  const [deadline, setDeadline] = useState("");
-  const [address, setEventAddress] = useState("");
-
-  const [eventStart, setEventStart] = useState("");
-  const [eventEnd, setEventEnd] = useState("");
-  const [repeating, setRepeating] = useState("");
-
-  const [categories, setEventCategories] = useState([]);
-
-  var center1 = {
-    lat: 45.42,
-    lng: -75.69,
-  };
-
-  const [center, setCenter] = useState(center1);
-
-  const handleOnClick = () => {
-    history.push("/new-opportunity");
-  };
-
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!,
-  });
-
-  useEffect(() => {
-    const getOpportunity = async () => {
-      const opportunityCollection = OpportunityCollection.create(config);
-      const data: any = await opportunityCollection.getOpportunities();
-
-      setOpportunity(data);
-      console.log(data);
-      if (data[0] != null) {
-        handleCardOnClick(0, 0, data[0]);
-      }
-      console.log("Data: ", data);
-
-      for (let i = 0; i < opportunities.length; i++) {
-        defaultIndexesBasedOnOpsLength[i] = false;
-      }
-      setIndexes(defaultIndexesBasedOnOpsLength);
-    };
-    getOpportunity();
-  }, []);
-
-  const handleCardOnClick = (e: any, i: any, props: any) => {
-    var dateInfo = new Date(props.date.seconds * 1000);
-    var deadlineInfo = new Date(props.deadline.seconds * 1000);
-
-    var center = {
-      lat: props.coordinates._lat,
-      lng: props.coordinates._long,
+    const handleOnClick = () => {
+        history.push("/new-opportunity");
     };
 
-    var formatedStartHour = "";
-    var formatedEndHour = "";
-    var formatedStartMinutes = "";
-    var formatedEndMinutes = "";
-    var startDate = new Date();
-    var endDate = new Date();
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!,
+    });
 
-    if (props.shift != null && props.shift[0] != null) {
-      console.log(startDate);
-      startDate = new Date(props.shift[0].start.seconds * 1000);
-      endDate = new Date(props.shift[0].end.seconds * 1000);
+    useEffect(() => {
+        const getOpportunity = async () => {
+            const opportunityCollection = OpportunityCollection.create(config);
+            const data: any = await opportunityCollection.getOpportunities();
 
-      startDate.getHours().toString().length === 1
-        ? (formatedStartHour = "0" + startDate.getHours())
-        : (formatedStartHour = "" + startDate.getHours());
-      endDate.getHours().toString().length === 1
-        ? (formatedEndHour = "0" + endDate.getHours())
-        : (formatedEndHour = "" + endDate.getHours());
-      startDate.getMinutes().toString().length === 1
-        ? (formatedStartMinutes = "0" + startDate.getMinutes())
-        : (formatedStartMinutes = "" + startDate.getMinutes());
-      endDate.getMinutes().toString().length === 1
-        ? (formatedEndMinutes = "0" + endDate.getMinutes())
-        : (formatedEndMinutes = "" + endDate.getMinutes());
+            setOpportunity(data);
 
-      setEventStart(
-        startDate.getMonth() +
-          "/" +
-          startDate.getDate() +
-          "/" +
-          startDate.getFullYear() +
-          " " +
-          formatedStartHour +
-          ":" +
-          formatedStartMinutes
-      );
-      setEventEnd(
-        endDate.getMonth() +
-          "/" +
-          endDate.getDate() +
-          "/" +
-          endDate.getFullYear() +
-          " " +
-          formatedEndHour +
-          ":" +
-          formatedEndMinutes
-      );
-      setDate(
-        dateInfo.getMonth() +
-          "/" +
-          dateInfo.getDate() +
-          "/" +
-          dateInfo.getFullYear()
-      );
-      setDeadline(
-        deadlineInfo.getMonth() +
-          "/" +
-          deadlineInfo.getDate() +
-          "/" +
-          deadlineInfo.getFullYear()
-      );
-      setRepeating(props.shift[0].recurring);
+            if (data[0] != null) {
+                handleCardOnClick(0, 0, data[0]);
+            }
+        };
+
+        getOpportunity();
+    }, []);
+
+    const formatDateTime = (date: Date) => {
+        const appendZero = (n: number) =>
+            n < 10 ? "0" + n.toString() : n.toString();
+
+        return `${
+            date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()} ${appendZero(
+            date.getHours()
+        )}:${appendZero(date.getMinutes())}`;
+    };
+
+    const formatDate = (date: Date) =>
+        `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+    const handleCardOnClick = (e: any, i: number, props: any) => {
+        setSelectedOpportunity(props);
+        setSelectedIndex(i);
+        setCenter({
+            lat: props.coordinates._lat,
+            lng: props.coordinates._long,
+        });
+    };
+
+    const ListEventCardComponents = opportunities.map(
+        (props: any, i: number) => {
+            const { eventName, eventImage, date, description, eventID } = props;
+            return (
+                <SmallEventCard
+                    key={eventID}
+                    eventImage={eventImage}
+                    eventName={eventName}
+                    date={date.toDate()}
+                    description={description}
+                    eventID={eventID}
+                    onClick={(e: any) => handleCardOnClick(e, i, props)}
+                    selected={i === selectedIndex}
+                />
+            );
+        }
+    );
+
+    if (!selectedOpportunity) {
+        return "";
     }
 
-    setPositionName(props.eventName);
-    setCompanyName(props.companyName);
-    setDescription(props.description);
-    setEventAddress(props.address);
-    setEventImage(props.eventImage);
-    setEventCategories(props.categories);
+    const {
+        eventName,
+        companyName,
+        description,
+        address,
+        eventImage,
+        categories,
+        shift,
+        deadline,
+        date,
+    } = selectedOpportunity;
 
-    setCenter(center);
-    const newIndexes = [false, false];
-    newIndexes[i] = true;
-    setIndexes(newIndexes);
-  };
-
-  const ListEventCardComponents = opportunities.map((props: any, i) => (
-    <SmallEventCard
-      eventImage={props.eventImage}
-      eventName={props.eventName}
-      date={new Date(props.date.seconds * 1000)}
-      description={props.description}
-      eventID={props.eventID}
-      onClick={(e: any) => handleCardOnClick(e, i, props)}
-      selected={indexes[i]}
-    ></SmallEventCard>
-  ));
-
-  return isLoaded ? (
-    <OrganizationDashboardPage>
-      <div>
+    const header = (
         <HeaderContainer>
-          <LeftBox>
-            <HeaderOne>Your organization dashboard</HeaderOne>
-            <HeaderFour>
-              Here you can view the volunteer opportunities you've posted, edit
-              them, or create a new one.
-            </HeaderFour>
-          </LeftBox>
-          <RightBox>
-            <Button onClick={handleOnClick}>Create a new opportunity</Button>
-          </RightBox>
+            <LeftBox>
+                <HeaderOne>Your organization dashboard</HeaderOne>
+                <HeaderFour>
+                    Here you can view the volunteer opportunities you've posted,
+                    edit them, or create a new one.
+                </HeaderFour>
+            </LeftBox>
+            <RightBox>
+                <Button onClick={handleOnClick}>
+                    Create a new opportunity
+                </Button>
+            </RightBox>
         </HeaderContainer>
+    );
 
-        <SignupContainer>
-          <Grid container>
-            <Grid item xs={6}>
-              <EventsListContainer>
-                {ListEventCardComponents}
-              </EventsListContainer>
-            </Grid>
+    const selectedOpportunityView = (
+        <SelectedOpportunity>
+            {eventImage ? (
+                <StyledImage src={eventImage} alt="EventImage" />
+            ) : (
+                <></>
+            )}
 
-            <Grid item xs={6}>
-              <Scolling>
-                {eventImage ? (
-                  <img
-                    src={eventImage}
-                    alt="EventImage"
-                    width="100%"
-                    height="300px"
-                  />
-                ) : (
-                  <></>
-                )}
-
-                <TextGroup>
-                  <Grid container>
+            <TextGroup>
+                <Grid container>
                     <Grid item xs={6}>
-                      <HeaderThree> {positionName} </HeaderThree>
-                      <HeaderTwo> {companyName} </HeaderTwo>
+                        <HeaderThree>{eventName}</HeaderThree>
+                        <HeaderTwo>{companyName}</HeaderTwo>
 
-                      {categories.map((category) => (
-                        <Location> {category} </Location>
-                      ))}
+                        {categories.map((category: any, i: number) => (
+                            <Location key={`category-${i}`}>
+                                {" "}
+                                {category}{" "}
+                            </Location>
+                        ))}
                     </Grid>
 
                     <Grid
-                      item
-                      xs={6}
-                      style={{ textAlign: "right", paddingRight: "50px" }}
+                        item
+                        xs={6}
+                        style={{
+                            textAlign: "right",
+                            paddingRight: "50px",
+                        }}
                     >
-                      <MoreOptions></MoreOptions>
-
-                      <SubHeader>
-                        <Calendar></Calendar>
-                        {date}
-                      </SubHeader>
-                      <SubHeader>Application Deadline: {deadline} </SubHeader>
+                        <MoreOptions />
+                        <SubHeader>
+                            <Calendar />
+                            {formatDate(new Date(date.seconds * 1000))}
+                        </SubHeader>
+                        <SubHeader>
+                            Application Deadline:{" "}
+                            {formatDate(new Date(deadline.seconds * 1000))}
+                        </SubHeader>
                     </Grid>
-                  </Grid>
-                </TextGroup>
+                </Grid>
+            </TextGroup>
 
-                <TextGroup style={{ paddingTop: "0px" }}>
-                  <HeaderTwo>Organization details</HeaderTwo>
-                  <Paragraph>{description}</Paragraph>
-                </TextGroup>
+            <TextGroup style={{ paddingTop: "0px" }}>
+                <HeaderTwo>Organization details</HeaderTwo>
+                <Paragraph>{description}</Paragraph>
+            </TextGroup>
 
-                <HeaderTwo>Location</HeaderTwo>
-                <SubHeader>{address}</SubHeader>
-                {center ? <Map center={center}></Map> : <></>}
+            <HeaderTwo>Location</HeaderTwo>
+            <SubHeader>{address}</SubHeader>
+            {center ? <Map center={center}></Map> : <></>}
 
+            {shift.map((s: any, i: number) => (
                 <TextGroup>
-                  <HeaderTwo>Shift 1</HeaderTwo>
-
-                  <Paragraph>Start: {eventStart} </Paragraph>
-                  <Paragraph>End: {eventEnd} </Paragraph>
-                  {repeating ? (
-                    <Paragraph>Does Not Repeat</Paragraph>
-                  ) : (
-                    <Paragraph>Repeats</Paragraph>
-                  )}
+                    <HeaderTwo>Shift {i + 1}</HeaderTwo>
+                    <Paragraph>
+                        Start:{" "}
+                        {formatDateTime(new Date(s.start.seconds * 1000))}
+                    </Paragraph>
+                    <Paragraph>
+                        End: {formatDateTime(new Date(s.end.seconds * 1000))}
+                    </Paragraph>
+                    {s.repeating ? (
+                        <Paragraph>Repeats</Paragraph>
+                    ) : (
+                        <Paragraph>Does Not Repeat</Paragraph>
+                    )}
                 </TextGroup>
+            ))}
 
-                <HeaderThree>Applicants</HeaderThree>
+            <HeaderThree>Applicants</HeaderThree>
 
-                <Paragraph>There are no applicants</Paragraph>
-              </Scolling>
-            </Grid>
-          </Grid>
-        </SignupContainer>
-      </div>
-    </OrganizationDashboardPage>
-  ) : (
-    <></>
-  );
+            <Paragraph>There are no applicants</Paragraph>
+        </SelectedOpportunity>
+    );
+
+    return isLoaded ? (
+        <OrganizationDashboardPage>
+            {header}
+
+            <SignupContainer>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <EventsListContainer>
+                            {ListEventCardComponents}
+                        </EventsListContainer>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        {selectedOpportunityView}
+                    </Grid>
+                </Grid>
+            </SignupContainer>
+        </OrganizationDashboardPage>
+    ) : (
+        <></>
+    );
 };
 
 const HeaderContainer = styled.div`
-  font-family: Source Sans Pro;
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  justify-content: space-between;
-  border-bottom: 2px solid silver;
-  margin-top: -1%;
-  flex-direction: row;
-  width: 1;
-  height: 0.3;
+    font-family: Source Sans Pro;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: space-between;
+    border-bottom: 2px solid silver;
+    margin-top: -1%;
+    flex-direction: row;
+    width: 1;
+    height: 0.3;
 `;
 
 const LeftBox = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  flex-direction: column;
-  width: 0.5;
-  padding-left: 2%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: column;
+    width: 0.5;
+    padding-left: 2%;
 `;
 
 const RightBox = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  flex-direction: column;
-  width: 0.5;
-  padding-top: 2%;
-  padding-right: 3%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: column;
+    width: 0.5;
+    padding-top: 2%;
+    padding-right: 3%;
 `;
 
 const HeaderOne = styled.h1`
@@ -315,73 +268,85 @@ const HeaderFour = styled.h3`
     margin-bottom: 1%;
 `;
 
-const Scolling = styled.div`
-  height: 70vh;
-  scroll-behaviour: smooth;
-  overflow-y: scroll;
-  overflow-x: hidden;
+const SelectedOpportunity = styled.div`
+    padding: 0px 14px;
+    height: 75vh;
+    background-color: white;
+    scroll-behaviour: smooth;
+    overflow-y: scroll;
+    overflow-x: hidden;
 `;
 
 const SignupContainer = styled.div`
-  font-family: Source Sans Pro;
-  width: fit-content;
-  margin: auto;
-  padding: 30px;
+    font-family: Source Sans Pro;
+    width: fit-content;
+    margin: auto;
+    background: rgba(117, 122, 124, 0.08);
+    overflow-y: hidden;
 `;
 
 const TextGroup = styled.div`
-  margin: auto;
-  padding-bottom: 20px;
-  padding-top: 20px;
+    margin: auto;
+    padding-bottom: 20px;
+    padding-top: 20px;
 `;
 
 const HeaderTwo = styled.p`
-  font-weight: bold;
-  margin: 0px;
+    font-weight: bold;
+    margin: 0px;
 `;
 
 const OrganizationDashboardPage = styled.div`
-  height: calc(100% - 85px);
+    height: calc(100% - 85px);
+`;
+
+const StyledImage = styled.img`
+    margin-top: 12px;
+    width: 100%;
+    height: 300px;
+    border-radius: 8px 8px 0px 0px;
 `;
 
 const EventsListContainer = styled.div`
-  height: 70vh;
-  font-family: Source Sans Pro;
-  overflow-y: scroll;
-  scroll-behaviour: smooth;
+    padding-right: 12px;
+    height: 75vh;
+    font-family: Source Sans Pro;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    scroll-behaviour: smooth;
 `;
 
 export const Location = styled.h4`
-  font-size: 14px;
-  font-weight: 400;
-  color: #736b6b;
-  margin: 0px 0 0 0;
-  display: inline-block;
-  padding: 5px;
-  padding-left: 0px;
+    font-size: 14px;
+    font-weight: 400;
+    color: #736b6b;
+    margin: 0px 0 0 0;
+    display: inline-block;
+    padding: 5px;
+    padding-left: 0px;
 `;
 
 export const SubHeader = styled.h4`
-  font-size: 14px;
-  font-weight: 400;
-  color: #736b6b;
-  margin: 0px 0 0 0;
+    font-size: 14px;
+    font-weight: 400;
+    color: #736b6b;
+    margin: 0px 0 0 0;
 `;
 
 export const Paragraph = styled.p`
-  font-size: 15px;
-  font-weight: 400;
-  color: #192226;
-  margin-bottom: 0px;
-  margin-top: 0px;
+    font-size: 15px;
+    font-weight: 400;
+    color: #192226;
+    margin-bottom: 0px;
+    margin-top: 0px;
 `;
 
 export const HeaderThree = styled.h3`
-  font-size: 20px;
-  font-weight: bold;
-  margin: 0px;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 0px;
 
-  color: #192226;
+    color: #192226;
 `;
 
 export default OrganizationDashboard;
